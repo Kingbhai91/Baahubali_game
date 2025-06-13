a619afdf8dfb09211e3da18b66eefe22d42dc17e
Kingbhai91 <sagargoodboy91@gmail.com>
2025-06-13 17:09:54 +0530

    Create baahubali.py
    Added baahubali.py  movent

---
+++ baahubali.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Screen setup
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Baahubali Game")
+
+# Load Baahubali image
+player_img = pygame.image.load("baahubali.png")
+player_img = pygame.transform.scale(player_img, (100, 150))
+player_x = 350
+player_y = 400
+player_speed = 5
+
+# Game loop
+running = True
+while running:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Movement control
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+
+    # Don't go off screen
+    player_x = max(0, min(player_x, 700))
+
+    screen.blit(player_img, (player_x, player_y))
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
